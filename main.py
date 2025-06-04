b5e67b9d5f1146dfcb1242e29db4780bb31b0a5f
Maqsud Boltayev <maqsud@Maqsuds-MacBook-Air.local>
2025-06-04 08:18:31 -0700

    bajarildi

--- main.py
+++ main.py
@@ -1,35 +1,77 @@
 from randomusers import randomusers_data
 
 def get_email_list(users: list[dict]) -> list[str]:
+    "Get list of email addresses from users"
     emails = []
     for user in users:
-        emails.append(user["email"])
+        emails.append(user['email'])
     return emails
 
-def get_name_list(users: list[dict]) -> list[str]:
-    pass
+results = randomusers_data["results"]
+email_list = get_email_list(results)
+print(email_list)
 
+def get_name_list(users: list[dict]) -> list[str]:
+    names =[]
+    for user in users :
+        names.append(user["name"])
+    return names
+
+names=randomusers_data["results"]
+name_list = get_name_list(names)
+print(name_list)
 def get_country_list(users: list[dict]) -> list[str]:
-    pass
+     countrys = []
+     for user in users :
+        countrys.append(user["location"]["country"])
+        return countrys
+
+countrys=randomusers_data["results"]
+country_list=get_country_list(countrys)
+print(country_list)
 
 def get_phone_list(users: list[dict]) -> list[str]:
-    pass
+    phones=[]
+    for user in users :
+        phones.append(user["phone"])
+        return phones
 
+phones=randomusers_data["results"]
+phone_list=get_phone_list(phones)
+print(phone_list)
 def get_city_list(users: list[dict]) -> list[str]:
-    pass
+    citys=[]
+    for user in users :
+        citys.append(user["location"]["city"])
+        return citys
+
+citys=randomusers_data["results"]
+city_list = get_city_list(citys)
+print(city_list)
 
 def get_oldest_user(users: list[dict]) -> dict:
-    pass
+    oldests=[]
+    for user in users :
+        oldests.append(user["dob"]["age"])
+    return oldests
+
+oldests=randomusers_data["results"]
+oldest_list=get_oldest_user(oldests)
+print(max(oldest_list))
 
 def get_youngest_user(users: list[dict]) -> dict:
-    pass
+    youngests=[]
+    for user in users :
+        youngests.append(user["dob"]["age"])
+    return youngests
+
+youngests=randomusers_data["results"]
+youngest_list=get_youngest_user(oldests)
+print(min(youngest_list))
 
 
 def main() -> None:
-    results = randomusers_data["results"]
-
-    email_list = get_email_list(results)
-    print(email_list)
+    print("hech narsa")
 
 if __name__ == "__main__":
     main()
